661398713846e91c2f64cbb4dd94c8a9e1be2c63
ge98lor <alexandre.cafaro@tum.de>
2020-01-16 23:32:47 +0100

    Html parser and extraction of heating/cooling load from Revit html reports, need to create database of reports and connect with the model

---
+++ EnergyReports/html_extractor.py
@@ -0,0 +1,62 @@
+import re
+
+"""Extactor of heating/cooling load and parameters for one building from html Revit report"""
+
+def html_single_extractor(text, get_keys = False):
+    dict_result = {}
+    index_temp = 0
+    index_cool = 0
+    index_heat = 0
+    wall_area = []
+    door_area = []
+
+    # Unique conditions for parameter and not particular to a building
+
+    for i in range(len(text)):
+        #Latitude
+        if 'Latitude' in text[i]:
+            dict_result['Latitude'] = int(re.findall(r'\d+', text[i+1])[0])
+        #Longitude
+        elif "Longitude" in text[i] :
+            dict_result['Longitude'] = int(re.findall(r'\d+', text[i+1])[0])
+        #Mean daily_range
+        elif text[i]=='Range':
+            dict_result['Mean_daily_range'] = int(re.findall(r'\d+', text[i+1])[0])
+        #Area
+        elif 'Family' in text[i] and 'Area' in text[i]:
+            dict_result['Area'] = int(re.findall(r'\d+', text[i+1])[0])
+        #Humidity
+        elif 'Humidity' in text[i]:
+            dict_result['Relative_humidity'] = int(re.findall(r'\d+', text[i])[0])
+        #Supply air temperature
+        elif text[i]=='Temperature' and index_temp==0 : #first temperature for supply air temperature
+            dict_result['Supply_air_temperature'] = int(re.findall(r'\d+', text[i+1])[0])
+            index_temp+=1
+        #Wall Area
+        elif 'Wall' in text[i] and text[i+1] == 'Area':
+            wall_area.append(int(re.findall(r'\d+', text[i+2])[0]))
+        #Door Area
+        elif 'Door' in text[i] and text[i+1] == 'Area':
+            door_area.append(int(re.findall(r'\d+', text[i+2])[0]))
+
+        #Peak cooling total load
+        elif text[i]=='Total' and text[i+1] == 'Load' and index_cool ==0:
+            dict_result['Cooling_load'] = int(re.findall(r'\d+', text[i+2])[0]+re.findall(r'\d+', text[i+2])[1])
+            index_cool+=1
+        #Peak heating load
+        elif text[i]=='Heating' and text[i+1] == 'Load' and index_heat ==0:
+            dict_result['Heating_load'] = int(re.findall(r'\d+', text[i+2])[0]+re.findall(r'\d+', text[i+2])[1])
+            index_heat+=1
+
+    dict_result['Wall_area'] = sum(wall_area)
+    dict_result['Door_area'] = sum(door_area)
+
+
+    keys = list(dict_result.keys())
+    items = list(dict_result.values())
+    # print(keys)
+
+    if get_keys :
+        return keys, items
+
+    return items
---
+++ EnergyReports/parser_main.py
@@ -0,0 +1,30 @@
+import csv
+import os
+import pandas as pd
+
+from html_extractor import html_single_extractor
+
+"""OPENING"""
+PATH = "/Users/alex/Desktop/ArchiBIM/DeepBIM/EnergyReports/Htm" #change to location of htm reports of Revit
+get_keys=True
+
+with open('bim_train.csv', mode='w') as bim_train:
+    bim_writer = csv.writer(bim_train, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for file in os.listdir(PATH):
+        if file.endswith(".htm"):
+            with open(os.path.join(PATH, file), "r", encoding='utf-16') as f:
+                raw_text= f.read()
+            text = raw_text.split()
+
+            if get_keys:
+                keys, items = html_single_extractor(text,get_keys)
+                bim_writer.writerow(keys)
+            else:
+                items = html_single_extractor(text,get_keys)
+
+            bim_writer.writerow(items)
+            get_keys = False
+
+# reader = pd.read_csv('bim_train.csv')
+# print(reader)
